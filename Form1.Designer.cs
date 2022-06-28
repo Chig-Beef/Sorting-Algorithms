3bcc025da925cef5ab44c8d779fe1ac5bc2cf8b8
Chig Beef <102199398+Chig-Beef@users.noreply.github.com>
2022-06-28 14:06:13 +0930

    Bogo sort, Bubble sort, Selection sort

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,7 +29,17 @@
         private void InitializeComponent()
         {
             this.lblTitle = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlBack = new System.Windows.Forms.Panel();
+            this.comSorts = new System.Windows.Forms.ComboBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.nudAmount = new System.Windows.Forms.NumericUpDown();
+            this.lblAmount = new System.Windows.Forms.Label();
+            this.nudMax = new System.Windows.Forms.NumericUpDown();
+            this.lblTop = new System.Windows.Forms.Label();
+            this.lblIter = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -41,25 +51,123 @@
             this.lblTitle.TabIndex = 0;
             this.lblTitle.Text = "SORTERS";
             // 
-            // panel1
+            // pnlBack
             // 
-            this.panel1.BackColor = System.Drawing.Color.SlateGray;
-            this.panel1.Location = new System.Drawing.Point(23, 73);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(741, 347);
-            this.panel1.TabIndex = 1;
+            this.pnlBack.BackColor = System.Drawing.Color.SlateGray;
+            this.pnlBack.Location = new System.Drawing.Point(23, 73);
+            this.pnlBack.Name = "pnlBack";
+            this.pnlBack.Size = new System.Drawing.Size(741, 347);
+            this.pnlBack.TabIndex = 1;
+            // 
+            // comSorts
+            // 
+            this.comSorts.FormattingEnabled = true;
+            this.comSorts.Items.AddRange(new object[] {
+            "Bubble Sort",
+            "Bogo Sort",
+            "Selection Sort"});
+            this.comSorts.Location = new System.Drawing.Point(791, 73);
+            this.comSorts.Name = "comSorts";
+            this.comSorts.Size = new System.Drawing.Size(181, 28);
+            this.comSorts.TabIndex = 2;
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(791, 327);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(181, 48);
+            this.btnSet.TabIndex = 3;
+            this.btnSet.Text = "Create New Array";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(791, 381);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(181, 48);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Sort\r\n";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // nudAmount
+            // 
+            this.nudAmount.Location = new System.Drawing.Point(866, 295);
+            this.nudAmount.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudAmount.Name = "nudAmount";
+            this.nudAmount.Size = new System.Drawing.Size(106, 26);
+            this.nudAmount.TabIndex = 5;
+            this.nudAmount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // lblAmount
+            // 
+            this.lblAmount.AutoSize = true;
+            this.lblAmount.Location = new System.Drawing.Point(791, 295);
+            this.lblAmount.Name = "lblAmount";
+            this.lblAmount.Size = new System.Drawing.Size(69, 20);
+            this.lblAmount.TabIndex = 6;
+            this.lblAmount.Text = "Amount:";
+            // 
+            // nudMax
+            // 
+            this.nudMax.Location = new System.Drawing.Point(866, 263);
+            this.nudMax.Name = "nudMax";
+            this.nudMax.Size = new System.Drawing.Size(106, 26);
+            this.nudMax.TabIndex = 7;
+            this.nudMax.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // lblTop
+            // 
+            this.lblTop.AutoSize = true;
+            this.lblTop.Location = new System.Drawing.Point(818, 265);
+            this.lblTop.Name = "lblTop";
+            this.lblTop.Size = new System.Drawing.Size(42, 20);
+            this.lblTop.TabIndex = 8;
+            this.lblTop.Text = "Max:";
+            // 
+            // lblIter
+            // 
+            this.lblIter.AutoSize = true;
+            this.lblIter.Location = new System.Drawing.Point(787, 24);
+            this.lblIter.Name = "lblIter";
+            this.lblIter.Size = new System.Drawing.Size(84, 20);
+            this.lblIter.TabIndex = 9;
+            this.lblIter.Text = "Iterations: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1013, 461);
+            this.Controls.Add(this.lblIter);
+            this.Controls.Add(this.lblTop);
+            this.Controls.Add(this.nudMax);
+            this.Controls.Add(this.lblAmount);
+            this.Controls.Add(this.nudAmount);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.comSorts);
+            this.Controls.Add(this.pnlBack);
             this.Controls.Add(this.lblTitle);
             this.Name = "Form1";
             this.Text = "Sorters";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMax)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,7 +176,15 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlBack;
+        private System.Windows.Forms.ComboBox comSorts;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.NumericUpDown nudAmount;
+        private System.Windows.Forms.Label lblAmount;
+        private System.Windows.Forms.NumericUpDown nudMax;
+        private System.Windows.Forms.Label lblTop;
+        private System.Windows.Forms.Label lblIter;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,9 +17,232 @@ namespace Sorter
             InitializeComponent();
         }
 
+        private static Random rnd { get; set; }
+        private static Bar[] bars { get; set; }
+        private static Bar[] extra_bars { get; set; }
+        private static int bar_width { get; set; }
+        private static int iter { get; set; }
+        enum sorts
+        {
+            Bubble,
+            Bogo,
+            Select
+        }
+
+
+        // Inbuilt Events
         private void Form1_Load(object sender, EventArgs e)
         {
+            iter = 0;
+            rnd = new Random();
+            this.Size = new Size(700, 400);
+            pnlBack.Size = new Size(500, 300);
+            nudAmount.Maximum = 500;
+        }
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            if (bars == null)
+            {
+                MessageBox.Show("Init array first", "Error");
+                return;
+            }
 
+            if (comSorts.SelectedIndex == -1)
+            {
+                MessageBox.Show("Pick a sorting algorithm", "Error");
+            }
+            
+            if (comSorts.SelectedIndex == Convert.ToInt32(sorts.Bubble))
+            {
+                Bubble_Sort();
+            }
+            else if (comSorts.SelectedIndex == Convert.ToInt32(sorts.Bogo))
+            {
+                Bogo_Sort();
+            }
+            else if (comSorts.SelectedIndex == Convert.ToInt32(sorts.Select))
+            {
+                extra_bars = new Bar[bars.Length];
+                Selection_Sort();
+            }
+        }
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            int amount = Convert.ToInt32(nudAmount.Value);
+            int max = Convert.ToInt32(nudMax.Value);
+            if (amount == 0)
+            {
+                MessageBox.Show("Invalid amount", "Error");
+                return;
+            }
+            if (max == 0 || max == 1)
+            {
+                MessageBox.Show("Invalid maximum", "Error");
+                return;
+            }
+
+            if (bars != null)
+            {
+                for (int i = 0; i < bars.Length; i++)
+                {
+                    bars[i].Pnl.Visible = false;
+                    Controls.Remove(bars[i].Pnl);
+                }
+            }
+
+            bars = new Bar[amount];
+            bar_width = pnlBack.Width / amount;
+            for (int i = 0; i < amount; i++)
+            {
+                bars[i] = new Bar(new Panel(), i, rnd.Next(1, max));
+                bars[i].Pnl.Parent = pnlBack;
+                bars[i].Pnl.Location = new Point(pnlBack.Left + bar_width * i, pnlBack.Top);
+                bars[i].Pnl.Size = new Size(bar_width, bars[i].Value*(pnlBack.Height/max));
+                bars[i].Pnl.BackColor = Color.Red;
+                Controls.Add(bars[i].Pnl);
+                bars[i].Pnl.BringToFront();
+            }
+        }
+
+        // Custom Functions
+        private void Bubble_Sort()
+        {
+            bool sorted = false;
+            Bar extra;
+            int iterations = 0;
+            while (!sorted)
+            {
+                for (int i = 0; i < bars.Length - 1 - iterations; i++)
+                {
+                    if (bars[i].Value > bars[i+1].Value)
+                    {
+                        extra = bars[i];
+                        bars[i] = bars[i + 1];
+                        bars[i + 1] = extra;
+                    }
+                }
+
+                iterations++;
+                lblIter.Text = "Iterations: " + iterations;
+
+                this.Update();
+
+                Update_Bars();
+
+                if (Check_Sorted())
+                {
+                    for (int i = 0; i < bars.Length; i++)
+                    {
+                        bars[i].Pnl.BackColor = Color.Green;
+                    }
+                    sorted = true;
+                }
+            }
+        }
+        private void Selection_Sort()
+        {
+            Bar extra;
+            bool sorted = false;
+            int iterations = 0;
+            int minumum;
+            int index = 0;
+            while (!sorted)
+            {
+                minumum = 1000;
+                for (int i = iterations; i < bars.Length; i++)
+                {
+                    if (bars[i].Value < minumum)
+                    {
+                        minumum = bars[i].Value;
+                        index = i;
+                    }
+                }
+
+                extra = bars[iterations];
+                bars[iterations] = bars[index];
+                bars[index] = extra;
+
+                iterations++;
+                lblIter.Text = "Iterations: " + iterations;
+
+                this.Update();
+
+                Update_Bars();
+
+                if (Check_Sorted())
+                {
+                    for (int i = 0; i < bars.Length; i++)
+                    {
+                        bars[i].Pnl.BackColor = Color.Green;
+                    }
+                    sorted = true;
+                }
+                if (iterations >= bars.Length)
+                {
+                    return;
+                }
+            }
+        }
+        private void Bogo_Sort()
+        {
+            int iterations = 0;
+            bool sorted = false;
+            while (!sorted)
+            {
+                bars = bars.OrderBy(x => rnd.Next()).ToArray();
+
+                iterations++;
+
+                lblIter.Text = "Iterations: " + iterations;
+
+                this.Update();
+
+                Update_Bars();
+
+                if (Check_Sorted())
+                {
+                    for (int i = 0; i < bars.Length; i++)
+                    {
+                        bars[i].Pnl.BackColor = Color.Green;
+                    }
+                    sorted = true;
+                }
+            }
+        }
+        private bool Check_Sorted()
+        {
+            for (int i = 0; i < bars.Length-1; i++)
+            {
+                if (bars[i].Value > bars[i+1].Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void Update_Bars()
+        {
+            for (int i = 0; i < bars.Length; i++)
+            {
+                bars[i].Index = i;
+                bars[i].Pnl.Location = new Point(pnlBack.Left + i * bar_width, pnlBack.Top);
+                bars[i].Pnl.BringToFront();
+            }
+        }
+
+        // Custom Classes
+        public class Bar
+        {
+            public Bar(Panel pnl, int ind, int vl)
+            {
+                Pnl = pnl;
+                Index = ind;
+                Value = vl;
+            }
+
+            public Panel Pnl { get; set; }
+            public int Index { get; set; }
+            public int Value { get; set; }
         }
     }
 }
